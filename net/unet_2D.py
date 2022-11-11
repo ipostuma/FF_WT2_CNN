ae97dfa1f767edc9ae7fc4491e5b93cbebc26a94
Ian Postuma <ian.postuma@gmail.com>
2022-11-11 09:45:12 +0100

    Update and rename unet.py to unet_2D.py

--- net/unet_2D.py
+++ net/unet_2D.py
@@ -3,27 +3,27 @@ keras.backend.clear_session()
 
 # down
 def down(previous_layer,k, **kargs):
-    d = keras.layers.Conv3D(k,3,padding="same",**kargs)(previous_layer)
+    d = keras.layers.Conv2D(k,3,padding="same",**kargs)(previous_layer)
     d = keras.layers.BatchNormalization()(d)
     d = keras.layers.Activation('relu')(d)
-    d = keras.layers.Conv3D(k,3,padding="same",**kargs)(d)
+    d = keras.layers.Conv2D(k,3,padding="same",**kargs)(d)
     d = keras.layers.BatchNormalization()(d)
     d = keras.layers.Activation('relu')(d)
-    d = keras.layers.Conv3D(k,3,padding="same",**kargs)(d)
+    d = keras.layers.Conv2D(k,3,padding="same",**kargs)(d)
     d = keras.layers.BatchNormalization()(d)
-    d = keras.layers.MaxPooling3D()(d)
+    d = keras.layers.MaxPooling2D()(d)
 
-    res = keras.layers.Conv3D(k,3,strides=2,padding="same",**kargs)(previous_layer)
+    res = keras.layers.Conv2D(k,3,strides=2,padding="same",**kargs)(previous_layer)
     d = keras.layers.Add()([res,d])
     d = keras.layers.Activation('relu')(d)
     return d
 
 # bottleneck
 def bottleneck(previous_layer,k, **kargs):
-    b = keras.layers.Conv3D(k*8,3,padding="same",**kargs)(previous_layer)
+    b = keras.layers.Conv2D(k,3,padding="same",**kargs)(previous_layer)
     b = keras.layers.BatchNormalization()(b)
     b = keras.layers.Activation('relu')(b)
-    b = keras.layers.Conv3D(k*8,3,padding="same",**kargs)(b)
+    b = keras.layers.Conv2D(k,3,padding="same",**kargs)(b)
     b = keras.layers.BatchNormalization()(b)
     b = keras.layers.Activation('relu')(b)
 
@@ -31,24 +31,24 @@ def bottleneck(previous_layer,k, **kargs):
 
 # up
 def up(previous_layer,down_layer,k, **kargs):
-    u = keras.layers.Conv3DTranspose(k,3,padding="same",**kargs)(previous_layer)
+    u = keras.layers.Conv2DTranspose(k,3,padding="same",**kargs)(previous_layer)
     u = keras.layers.BatchNormalization()(u)
     u = keras.layers.Activation('relu')(u)
     u = keras.layers.Concatenate(axis=-1)([u,down_layer])
-    u = keras.layers.Conv3D(k,3,padding="same",**kargs)(u)
+    u = keras.layers.Conv2D(k,3,padding="same",**kargs)(u)
     u = keras.layers.BatchNormalization()(u)
     u = keras.layers.Activation('relu')(u)
-    u = keras.layers.Conv3D(k,3,padding="same",**kargs)(u)
+    u = keras.layers.Conv2D(k,3,padding="same",**kargs)(u)
     u = keras.layers.BatchNormalization()(u)
-    u = keras.layers.UpSampling3D()(u)
+    u = keras.layers.UpSampling2D()(u)
 
-    res = keras.layers.UpSampling3D()(previous_layer)
-    res = keras.layers.Conv3D(k,1,padding="same",**kargs)(res)
+    res = keras.layers.UpSampling2D()(previous_layer)
+    res = keras.layers.Conv2D(k,1,padding="same",**kargs)(res)
     u = keras.layers.Add()([u,res])
     u = keras.layers.Activation('relu')(u)
     return u
 
-def model(input_shape=(64,64,64,1),k1=8,reg=0.1):
+def model(input_shape=(384,192,17,1),k1=8,reg=0.1):
     inputs=keras.layers.Input(shape=(input_shape))
     
     params = {"kernel_regularizer" : keras.regularizers.l2(reg), 
